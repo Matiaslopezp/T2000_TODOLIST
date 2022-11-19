03879f3c1079d8d1ae5e2f8dfa8a03e9b45e512e
Matiaslopezp <lopezp.matias@gmail.com>
2022-11-19 14:58:47 +0000

    se realizó solo el css a medias

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,20 +6,19 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container"> 
+		<h1>Hola</h1>
+		<ul>
+			<li>Hacer ejercicio</li>
+			<li>Leer</li>
+			<li>Ir de compras al supermercado</li>
+			<li>Caminar por el parque</li>	
+			<li>Jugar con mi mascota</li>	
+		</ul>
+		
 		</div>
+
 	);
 };
 
